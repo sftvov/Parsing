6482122107af8e575bb78f7dd68c2f5f906cb3e4
sftvov <63662363+sftvov@users.noreply.github.com>
2023-12-29 20:12:59 +0300

    29122023

--- parser.js
+++ parser.js
@@ -3,6 +3,28 @@ const { log } = require('console');
 const fs = require('fs'); // Подключение встроенного в Node.js модуля fs для работы с файловой системой
 const link = 'https://store.tildacdn.com/api/getproductslist/?storepartuid=433685419311&recid=236139262&c=1687441112429&getparts=true&getoptions=true&slice=1&sort;5Bcreated;5D=desc&size=64';
 
+function renameCats(str) {
+	const obj = {
+		varezhki: 'Одежда для города > Варежки',
+		teplogrejki: 'Одежда для катания > Теплогрейки',
+		shtany: 'Одежда для города > Штаны',
+		polukombinezony: 'Одежда для катания > Полукомбинезоны',
+		panamy: 'Одежда для города > Панамы',
+		kurtki: 'Одежда для катания > Куртки',
+		hudi: 'Одежда для катания > Худи',
+		hudisnow: 'Одежда для катания > Худи',
+		anoraki: 'Одежда для катания > Анораки',
+	};
+
+	const entries = Object.entries(obj);
+
+	for (const name of entries) {
+		if (str === name[0]) str = name[1];
+	}
+
+	return str;
+}
+
 function parsing() {
 	axios
 		.get(link)
@@ -11,36 +33,38 @@ function parsing() {
 			const products = response.data.products;
 			let row = 0;
 			// console.log(products[0]);
-			for (let name = 0; name < products.length; name++) {
+			for (let i = 0; i < products.length; i++) {
 				data[row] = [];
-				data[row].push(products[name].uid);
+				data[row].push(products[i].uid);
 				data[row].push('');
-				data[row].push(products[name].title);
-				data[row].push(products[name].descr);
+				data[row].push(products[i].title);
+				data[row].push(products[i].descr);
 				function replaceAll(string, search, replace) {
 					return string.split(search).join(replace);
 				}
-				const text = replaceAll(products[name].text, '%', ' процентов');
+				const text = replaceAll(products[i].text, '%', ' процентов');
 				data[row].push(text);
 				data[row].push('');
 				data[row].push('');
-				data[row].push(JSON.parse(products[name].gallery).map((o) => o.img));
-				let url = replaceAll(products[name].url, 'catalog/', '');
+				data[row].push(JSON.parse(products[i].gallery).map((o) => o.img));
+				let url = replaceAll(products[i].url, 'catalog/', '');
 				url = replaceAll(url, 'tproduct/', '');
-				console.log(name);
-				data[row].push(url.match(/https:\/\/xvoybrand\.ru\/?(\w*)\//)[1]);
+				console.log(i);
+				data[row].push(renameCats(url.match(/https:\/\/xvoybrand\.ru\/?(\w*)\//)[1]));
 				let attr1 = {};
 				let attr2 = {};
-				if (products[name].hasOwnProperty('json_options') && products[name].json_options[0]) {
-					attr1 = JSON.parse(products[name].json_options)[0];
+				if (products[i].hasOwnProperty('json_options') && products[i].json_options[0]) {
+					attr1 = JSON.parse(products[i].json_options)[0];
 					data[row].push(attr1.title);
 					data[row].push(attr1.values);
 					data[row].push(attr1.values[0]);
-					if (JSON.parse(products[name].json_options)[1]) {
-						attr2 = JSON.parse(products[name].json_options)[1];
+					data[row].push(1);
+					if (JSON.parse(products[i].json_options)[1]) {
+						attr2 = JSON.parse(products[i].json_options)[1];
 						data[row].push(attr2.title);
 						data[row].push(attr2.values);
 						data[row].push(attr2.values[0]);
+						data[row].push(1);
 					} else {
 						attr2 = '';
 						data[row].push('');
@@ -53,16 +77,16 @@ function parsing() {
 					data[row].push('');
 					data[row].push('');
 				}
-				if (products[name].editions) {
+				if (products[i].editions) {
 					data[row].push('variable');
 					data[row].push('');
 					row++;
-					let ed = products[name].editions;
+					let ed = products[i].editions;
 					for (let i2 = 0; i2 < ed.length; i2++) {
 						let num = row + i2;
 						data[num] = [];
 						data[num].push(ed[i2].uid);
-						data[num].push(products[name].uid);
+						data[num].push(products[i].uid);
 						data[num].push('');
 						data[num].push('');
 						data[num].push('');
